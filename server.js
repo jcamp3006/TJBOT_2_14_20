7d58d56f533d2b758e6d35071a6b4b8baa48a52e
jcamp3006 <jonathan.campbell@ibm.com>
2020-02-20 13:41:25 -0600

    Changed port to process or 8080

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const port = process.env.PORT || 8080;
+
 
 app.use('/pages', express.static(path.join(__dirname, 'pages')));
 
 
-app.listen(8080, () => {
-  console.log("Listening on port : 8080");
+app.listen(port, () => {
+  console.log("Listening on port : " + port);
 });
