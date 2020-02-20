8d3969b808f34f816973382dec6350b2414d4d59
jcamp3006 <jonathan.campbell@ibm.com>
2020-02-20 09:15:50 -0600

    changed port to 8080

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const path = require('path');
 app.use('/pages', express.static(path.join(__dirname, 'pages')));
 
 
-app.listen(5000, () => {
+app.listen(8080, () => {
   console.log("Listening on port : 5000");
 });
